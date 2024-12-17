0607e5f0e0b3b4a44f9cd1474fd87a46683f4e7e
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2024-12-17 22:59:14 +0500

    update main 1.0

--- labalinux/labalinux.cpp
+++ labalinux/labalinux.cpp
@@ -8,5 +8,43 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "RUS");
+    int A, B;
+    int zero = 0;
+    int one = 0;
+    int two = 0;
+    bool pop = false;
     
+    int number = randomnumber();
+    int* f=new int[4]; int dop = number;
+    for (int i = 3; i>=0; i--) {
+        f[i] = dop % 10;
+        dop /= 10;
+    }
+    cout << "Введите четырёхзначное число - ";
+
+    while (pop == false) {
+        int l = 3;
+        cin >> B;
+        cout << endl;
+        while (B > 0) {
+            int N = proverka(f, B % 10,l);
+            if (N == 0) {
+                one++;
+            }
+            else if (N == 1) {
+                two++;
+            }
+            B /= 10;
+            l--;
+        }
+        if (two == 4) {
+            cout << "Правильно! Вы выиграли! Ура!";
+            pop = true;
+        }
+        else {
+            cout << "Вы на пути к правильному ответу! Коров - " << one << " , а быков - " << two<<endl;
+            one = 0; two = 0;
+        }
+    }
 }
