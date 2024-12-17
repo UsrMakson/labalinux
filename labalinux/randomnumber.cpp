687170a1dc69a138fdec1a2c145fa51d128de66a
Бочкарев ИТ-16 <81972957+AdReLaN@users.noreply.github.com>
2024-12-17 23:07:42 +0500

    Генерация рандомного числа

--- labalinux/randomnumber.cpp
+++ labalinux/randomnumber.cpp
@@ -2,6 +2,16 @@
 #include <random>
 
 int randomnumber() {
-	
-	return 0;
+	srand(time(0));
+	int f[]{ 1,2,3,4,5,6,7,8,9 }; int d = 1; int ind = rand() % 9;
+	int number = 0;
+	for (int i = 0; i < 4; i++) {
+		while (f[ind] == 0) {
+			ind = rand() % 9;
+		}
+		number = number + d * f[ind];
+		d *= 10;
+		f[ind] = 0;
+	}
+	return number;
 }
